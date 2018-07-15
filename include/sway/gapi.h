f989ad4966a823b45fba1758848f62fb239fad2f
Victor Timoshin <victor-timoshin@hotmail.com>
2018-07-15 11:45:38 +0300

    Добавлен BufferAccess_t перечислитель

---
+++ include/sway/gapi.h
@@ -0,0 +1,6 @@
+#ifndef SWAY_GAPI_H
+#define SWAY_GAPI_H
+
+#include <sway/gapi/bufferaccesses.h>
+
+#endif // SWAY_GAPI_H
---
+++ include/sway/gapi/bufferaccesses.h
@@ -0,0 +1,19 @@
+#ifndef SWAY_GAPI_BUFFERACCESSES_H
+#define SWAY_GAPI_BUFFERACCESSES_H
+
+#include <sway/namespacemacros.h>
+
+NAMESPACE_BEGIN(sway)
+NAMESPACE_BEGIN(gapi)
+
+enum class BufferAccess_t : u8_t {
+	kNone,
+	kRead,
+	kWrite,
+	kReadWrite
+};
+
+NAMESPACE_END(gapi)
+NAMESPACE_END(sway)
+
+#endif // SWAY_GAPI_BUFFERACCESSES_H
