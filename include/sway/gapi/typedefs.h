a429719970fd766654d395c5fd96cad7152756d9
Victor Timoshin <victor-timoshin@hotmail.com>
2019-04-25 11:30:38 +0300

    Заменены все boost на std

--- include/sway/gapi/typedefs.h
+++ include/sway/gapi/typedefs.h
@@ -10,8 +10,9 @@
 #include <initializer_list> // std::initializer_list
 #include <utility> // std::pair
 
-#include <boost/shared_ptr.hpp> // boost::shared_ptr
-#include <boost/unordered_map.hpp> // boost::unordered_map
+#include <functional> // std::function
+#include <memory> // std::shared_ptr
+#include <unordered_map> // std::unordered_map
 
 NAMESPACE_BEGIN(sway)
 NAMESPACE_BEGIN(gapi)
@@ -19,19 +20,19 @@ NAMESPACE_BEGIN(gapi)
 typedef std::set<u32_t> ShaderObjectIdSet_t;
 typedef std::initializer_list<std::pair<lpcstr_t, lpcstr_t>> ExtensionInitList_t;
 
-typedef boost::shared_ptr<class ICapabilityBase> CapabilityRef_t;
-typedef boost::shared_ptr<class AShaderBase> ShaderRef_t;
-typedef boost::shared_ptr<class AShaderProgramBase> ShaderProgramRef_t;
-typedef boost::shared_ptr<class ATextureBase> TextureRef_t;
-typedef boost::shared_ptr<class ABufferBase> BufferRef_t;
-typedef boost::shared_ptr<class IVertexLayoutBase> VertexLayoutRef_t;
-typedef boost::shared_ptr<class IViewportBase> ViewportRef_t;
-typedef boost::shared_ptr<class IDrawCallBase> DrawCallRef_t;
+typedef std::shared_ptr<class ICapabilityBase> CapabilityRef_t;
+typedef std::shared_ptr<class AShaderBase> ShaderRef_t;
+typedef std::shared_ptr<class AShaderProgramBase> ShaderProgramRef_t;
+typedef std::shared_ptr<class ATextureBase> TextureRef_t;
+typedef std::shared_ptr<class ABufferBase> BufferRef_t;
+typedef std::shared_ptr<class IVertexLayoutBase> VertexLayoutRef_t;
+typedef std::shared_ptr<class IViewportBase> ViewportRef_t;
+typedef std::shared_ptr<class IDrawCallBase> DrawCallRef_t;
 
-typedef boost::function<void (BufferRef_t)> DrawCbFunc_t;
+typedef std::function<void (BufferRef_t)> DrawCbFunc_t;
 
-typedef boost::unordered_map<std::string, math::vec4f_t> UniformVec4fUmap_t;
-typedef boost::unordered_map<std::string, struct VertexAttributeDescriptor> VertexAttribDescUmap_t;
+typedef std::unordered_map<std::string, math::vec4f_t> UniformVec4fUmap_t;
+typedef std::unordered_map<std::string, struct VertexAttributeDescriptor> VertexAttribDescUmap_t;
 
 NAMESPACE_END(gapi)
 NAMESPACE_END(sway)
