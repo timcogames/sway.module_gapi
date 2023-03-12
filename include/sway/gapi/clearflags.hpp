6831dbd67a8340132d0ce9f8d44173af5e4a7bb7
apriori85 <victor-timoshin@hotmail.com>
2023-03-12 12:26:23 +0300

    Added ClearFlag  operators

--- include/sway/gapi/clearflags.hpp
+++ include/sway/gapi/clearflags.hpp
@@ -10,6 +10,14 @@ NAMESPACE_BEGIN(gapi)
 
 enum class ClearFlag : u8_t { COLOR = ENUM_BITMASK(0), DEPTH = ENUM_BITMASK(1), STENCIL = ENUM_BITMASK(2) };
 
+inline constexpr ClearFlag operator&(ClearFlag l, ClearFlag r) {
+  return static_cast<ClearFlag>(static_cast<u8_t>(l) & static_cast<u8_t>(r));
+}
+
+inline constexpr ClearFlag operator|(ClearFlag l, ClearFlag r) {
+  return static_cast<ClearFlag>(static_cast<u8_t>(l) | static_cast<u8_t>(r));
+}
+
 NAMESPACE_END(gapi)
 NAMESPACE_END(sway)
 
