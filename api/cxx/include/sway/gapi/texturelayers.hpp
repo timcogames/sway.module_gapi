8df265a879fa5ceb2905701d28a7f5cb85ea18ae
apriori85 <victor-timoshin@hotmail.com>
2024-07-20 19:47:01 +0300

    Fixed

--- api/cxx/include/sway/gapi/texturelayers.hpp
+++ api/cxx/include/sway/gapi/texturelayers.hpp
@@ -8,7 +8,7 @@ NAMESPACE_BEGIN(gapi)
 
 enum class TextureLayer : u32_t { NONE = 0, DIFFUSE, SPECULAR, EMISSIVE, NORMAL, Latest };
 
-#define NUM_TEXTURE_LAYERS (core::detail::toUnderlying(TextureLayer::Latest))
+#define NUM_TEXTURE_LAYERS (core::detail::toBase(TextureLayer::Latest))
 
 NAMESPACE_END(gapi)
 NAMESPACE_END(sway)
