eb1c889d5ce93e42d933557e7e2153e6435336a7
apriori85 <victor-timoshin@hotmail.com>
2024-09-29 22:15:41 +0300

    Renamed newGuid to getNextUid

--- api/cxx/include/sway/gapi/idgenerator.hpp
+++ api/cxx/include/sway/gapi/idgenerator.hpp
@@ -13,7 +13,7 @@ class IdGenerator {
 public:
   virtual ~IdGenerator() = default;
 
-  PURE_VIRTUAL(auto newGuid() -> u32_t);
+  PURE_VIRTUAL(auto getNextUid() -> u32_t);
 };
 
 NAMESPACE_END(gapi)
